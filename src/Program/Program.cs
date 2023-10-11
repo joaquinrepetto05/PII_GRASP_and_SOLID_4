7a093c1ca1075a6688bd89a763fd0bb3388ef906
Joaquin Repetto <joaquin.repetto@correo.ucu.edu.uy>
2023-10-11 09:03:24 -0300

    .

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -12,66 +12,66 @@ namespace Full_GRASP_And_SOLID
 {
     public class Program
     {
-        private static List<Product> productCatalog = new List<Product>();
+        private static List<Product> productCatalog = new List<Product> ();
 
-        private static List<Equipment> equipmentCatalog = new List<Equipment>();
+        private static List<Equipment> equipmentCatalog = new List<Equipment> ();
 
         public static void Main(string[] args)
         {
-            PopulateCatalogs();
+            PopulateCatalogs ();
 
-            Recipe recipe = new Recipe();
-            recipe.FinalProduct = GetProduct("Café con leche");
-            recipe.AddStep(new Step(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120));
-            recipe.AddStep(new Step(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60));
+            Recipe recipe = new Recipe ();
+            recipe.FinalProduct = GetProduct ("Café con leche");
+            recipe.AddStep (new Step (GetProduct ("Café"), 100, GetEquipment ("Cafetera"), 120));
+            recipe.AddStep (new Step (GetProduct ("Leche"), 200, GetEquipment ("Hervidor"), 60));
 
             IPrinter printer;
-            printer = new ConsolePrinter();
-            printer.PrintRecipe(recipe);
-            printer = new FilePrinter();
-            printer.PrintRecipe(recipe);
+            printer = new ConsolePrinter ();
+            printer.PrintRecipe (recipe);
+            printer = new FilePrinter ();
+            printer.PrintRecipe (recipe);
         }
 
-        private static void PopulateCatalogs()
+        private static void PopulateCatalogs ()
         {
-            AddProductToCatalog("Café", 100);
-            AddProductToCatalog("Leche", 200);
-            AddProductToCatalog("Café con leche", 300);
+            AddProductToCatalog ("Café", 100);
+            AddProductToCatalog ("Leche", 200);
+            AddProductToCatalog ("Café con leche", 300);
 
-            AddEquipmentToCatalog("Cafetera", 1000);
-            AddEquipmentToCatalog("Hervidor", 2000);
+            AddEquipmentToCatalog ("Cafetera", 1000);
+            AddEquipmentToCatalog ("Hervidor", 2000);
         }
 
-        private static void AddProductToCatalog(string description, double unitCost)
+        private static void AddProductToCatalog (string description, double unitCost)
         {
-            productCatalog.Add(new Product(description, unitCost));
+            productCatalog.Add (new Product (description, unitCost));
         }
 
-        private static void AddEquipmentToCatalog(string description, double hourlyCost)
+        private static void AddEquipmentToCatalog (string description, double hourlyCost)
         {
-            equipmentCatalog.Add(new Equipment(description, hourlyCost));
+            equipmentCatalog.Add (new Equipment (description, hourlyCost));
         }
 
-        private static Product ProductAt(int index)
+        private static Product ProductAt (int index)
         {
-            return productCatalog[index] as Product;
+            return productCatalog [index] as Product;
         }
 
-        private static Equipment EquipmentAt(int index)
+        private static Equipment EquipmentAt (int index)
         {
-            return equipmentCatalog[index] as Equipment;
+            return equipmentCatalog [index] as Equipment;
         }
 
-        private static Product GetProduct(string description)
+        private static Product GetProduct (string description)
         {
             var query = from Product product in productCatalog where product.Description == description select product;
-            return query.FirstOrDefault();
+            return query.FirstOrDefault ();
         }
 
-        private static Equipment GetEquipment(string description)
+        private static Equipment GetEquipment (string description)
         {
             var query = from Equipment equipment in equipmentCatalog where equipment.Description == description select equipment;
-            return query.FirstOrDefault();
+            return query.FirstOrDefault ();
         }
     }
 }
